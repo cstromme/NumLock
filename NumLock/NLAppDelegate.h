54f5872309d0e0ca274fcbe2fde687fde562f2b7
Christian A. Strømmen <cstromme@Callisto-2.local>
2013-03-07 18:12:30 +0100

    Initial Commit

---
+++ NumLock/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  NumLock
+//
+//  Created by Christian A. Strømmen on 07.03.13.
+//  Copyright (c) 2013 Object Factory. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NumLock/NLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  NLAppDelegate.m
+//  NumLock
+//
+//  Created by Christian A. Strømmen on 07.03.13.
+//  Copyright (c) 2013 Object Factory. All rights reserved.
+//
+
+#import "NLAppDelegate.h"
+
+@implementation NLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
